455bcf70c885bffe6e6f4e2af01a8b07d6d9515c
Gaurav <gauravp1335@gmail.com>
2023-08-07 21:51:08 +0530

    Added products

---
+++ script/products.js
@@ -0,0 +1,206 @@
+var products=[
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/594123.png?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "La Cappuccina Sauvignon Blanc VNS",
+       price : "2860",
+       category : "White Wine",
+       region : "Italy"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/485479.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Presqu'ile Pinot Noir ",
+       price : "5890",
+       category : "Red Wine",
+       region : "California"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/592151.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "90+ Cellars Lot 75 Pinot Noir 2019",
+       price : "3590",
+       category : "Red Wine",
+       region : "California"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/623751.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Adaptation Cabernet Sauvignon 2018",
+       price : "4990",
+       category : "Red Wine",
+       region : "California"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/640093.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Amity Pinot Noir",
+       price : "2400",
+       category : "Red Wine",
+       region : "USA"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/627517.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Apothic Winemaker's Blend Red",
+       price : "2860",
+       category : "Red Wine",
+       region : "California"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/524364.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Alois Lageder Pinot Grigio",
+       price : "4990",
+       category : "White Wine",
+       region : "Italy"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/559284.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Argiolas Costamolino",
+       price : "7890",
+       category : "White Wine",
+       region : "Italy"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/587109.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Burgans Albariño",
+       price : "1760",
+       category : "White Wine",
+       region : "Spain"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/591550.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Domaine Savary Chablis Vieilles Vignes",
+       price : "5970",
+       category : "White Wine",
+       region : "France"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/578735.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "90+ Cellars Lot 50 Prosecco",
+       price : "1990",
+       category : "Sparkling Wine",
+       region : "Italy"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/628145.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Barefoot Bubbly Brut Cuvee",
+       price : "5860",
+       category : "Sparkling Wine",
+       region : "California"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/590460.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Billecart-Salmon Brut Nature",
+       price : "19990",
+       category : "Sparkling Wine",
+       region : "France"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/593095.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : " Billecart-Salmon Brut Réserve",
+       price : "7950",
+       category : "Sparkling Wine",
+       region : "France"
+    },
+    {
+       img_url:"https://icdn.bottlenose.wine/images/full/547761.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+       name : "Bodegas Dios Baco Amontillado",
+       price : "6960",
+       category : "Dessert Wine",
+       region : "Spain"
+    },
+    {
+      img_url:"https://icdn.bottlenose.wine/images/full/552730.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Juve y Camps Cava Brut Rosé",
+      price : "3550",
+      category : "Sparkling Wine",
+      region : "Spain"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/507252.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : " Juve y Camps Reserva de la Familia Brut Nature",
+      price : "3290",
+      category : "Sparkling Wine",
+      region : "Spain"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/578521.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : " La Marca Prosecco",
+      price : "2950",
+      category : "Sparkling Wine",
+      region : "Italy"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/531395.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Carpano Antica Formula Vermouth",
+      price : "7960",
+      category : "Dessert Wine",
+      region : "Italy"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/553192.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Florio Sweet Marsala",
+      price : "1960",
+      category : "Dessert Wine",
+      region : "Italy"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/592463.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Fonseca Bin No. 27 Finest Reserve Porto",
+      price : "5900",
+      category : "Dessert Wine",
+      region : "Portugal"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/238824.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Kopke Fine Ruby Port",
+      price : "6960",
+      category : "Dessert Wine",
+      region : "Portugal"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/592464.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Taylor Fladgate Tawny Port 20 year old",
+      price : "5780",
+      category : "Dessert Wine",
+      region : "Portugal"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/557934.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Taylor Fladgate Tawny Port 10 year old",
+      price : "5780",
+      category : "Dessert Wine",
+      region : "Portugal"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/560353.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Warre's Fine White Port",
+      price : "2930",
+      category : "Dessert Wine",
+      region : "France"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/474165.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Beringer Main & Vine White Zinfandel",
+      price : "2480",
+      category : "Rose Wine",
+      region : "California"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/564127.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Bieler Pere et Fils Sabine Rosé",
+      price : "3780",
+      category : "Rose Wine",
+      region : "France"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/470580.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Buckle Family Wine Colorado Rose",
+      price : "4950",
+      category : "Rose Wine",
+      region : "USA"
+   },
+   {
+      img_url:"https://icdn.bottlenose.wine/images/full/189600.jpg?ar=1&fit=fill&fill=solid&fill-color=fff&auto=format&ixlib=imgixjs-3.6.1&w=134",
+      name : "Joseph Cattin Crémant d'Alsace Rosé",
+      price : "6780",
+      category : "Rose Wine",
+      region : "France"
+   }
+]
+
