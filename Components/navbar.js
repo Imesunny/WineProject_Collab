e074b52d3a9840d9ca15bb75ac4c3465a8186eb0
heyvipul <vipulgirhestar@gmail.com>
2023-08-09 14:50:56 +0530

    Updated90%

--- Components/navbar.js
+++ Components/navbar.js
@@ -9,17 +9,18 @@ function navbar(){
             <h4>0 800 123 456</h4>    
         </div>
         <div id="right">
-            <span><i class="fa-solid fa-user"></i></span>
+            <span><a href="login.html"><i class="fa-solid fa-user"></i></a></span>
             <span><i class="fa-solid fa-scale-unbalanced"></i></span>
-            <span><i class="fa-solid fa-heart"></i></span>
-            <span><i class="fa-solid fa-bag-shopping"></i></span>
+            <span><a href="like.html"><i class="fa-solid fa-heart"></i></a></span>
+            <span><a href="cart.html"><i class="fa-solid fa-bag-shopping"></i></a></span>
         </div>
     </div> 
     <hr id="hr1">  
     <div id="lower_nav">
         
         <div><h1>Mine Wine</h1></div>
-        <div>
+        
+        <div id="nav_middle" >
             <h4>RED</h4>
             <h4>WHITE</h4>
             <h4>ROSE</h4>
